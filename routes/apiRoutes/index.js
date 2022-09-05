288b8a9ddda5f5a7174183661e02d383dff27b0c
Sam Karp <sfcypret@gmail.com>
2022-09-05 17:46:35 -0400

    attempting to use middleware to connect basic api routes and test response

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,1 +1,8 @@
 //middleware file for api notes
+const router = require('express').Router();
+//tell express how to handle data in noteRoute.js
+const noteRoute = require('./noteRoute');
+
+router.use(noteRoute);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 //heroku: https://floating-river-07736.herokuapp.com/
 const express = require('express');
 
-//route created for front end request to json data file
-const notes = require('./Develop/db/db.json');
+// //route created for front end request to json data file
+// const notes = require('./Develop/db/db.json');- moved to noteroute for api
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
 
 
 //route to api
 //http://localhost:3001/api/notes works
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    console.log(req.query)
-    res.json(results);
-});
+//moved to api route noteRoute.js
+// app.get('/api/notes', (req, res) => {
+//     let results = notes;
+//     console.log(req.query)
+//     res.json(results);
+// });
 
-
+app.use('/api/notes', apiRoutes);
 
 
 app.listen(PORT, () => {

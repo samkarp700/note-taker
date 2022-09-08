2c49928a28dcfce37bfbc12da20c3c8d51dbd753
Sam Karp <sfcypret@gmail.com>
2022-09-07 20:12:56 -0400

    file changes, html route set up, api route set up to server

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,1 +1,14 @@
 //router for html content
+const router = require('express').Router();
+const path = require('path');
+
+router.get('/notes', (req, res) => {
+    let filePath = path.join(__dirname, '../../public/notes.html');
+    res.sendFile(filePath);
+})
+router.get('/', (req,res) => {
+    let noteHome = path.join(__dirname,'../../public/index.html');
+    res.sendFile(noteHome);
+})
+
+module.exports = router;

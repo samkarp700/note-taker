49422dbcda32c04f5bd324543a2bf40e1d40bd16
Sam Karp <sfcypret@gmail.com>
2022-09-08 20:01:48 -0400

    finally fixed console error, create lib folder again to throw static functions in, reworked routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,28 +4,7 @@ const notes = require('../../db/db.json');
 const uuid = require('../../helpers/uuid');
 const fs = require('fs');
 const path = require('path');
-
-
-//functions needed
-
-function showAllNotes(query, notesArray) {
-    let filteredNotes = notesArray
-    if (query.title) {
-        filteredNotes = filteredNotes.filter(
-            (note) => note.title === query.title
-        )
-    } return filteredNotes
-}
-
-//create note function
-function createNote(body, notesArray) {
-    const note = body
-    notesArray.push(note)
-    fs.writeFileSync(
-        path.join(__dirname, '../../db/db.json'), 
-        JSON.stringify({notes: notesArray}, null, 2),
-    ); return note
-};
+const { showAllNotes, createNote, findbyId } = require('../../lib/index');
 
 
 
--- server.js
+++ server.js
@@ -28,6 +28,8 @@ app.use('/', htmlRoutes);
 
 
 
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
